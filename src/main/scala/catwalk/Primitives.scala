70fec68176a067585055d263584916f328e046c2
Robert Koeninger <rkoeninger@att.net>
2017-04-16 15:40:41 -0400

    Added more primitives

--- src/main/scala/catwalk/Primitives.scala
+++ src/main/scala/catwalk/Primitives.scala
@@ -9,8 +9,50 @@ object Primitives {
     case (_, _ :: stack) => stack
     case _ => throw new StackUnderflowException()
   }
+  val duplicate: (Environment, List[Value]) => List[Value] = {
+    case (_, x :: stack) => x :: x :: stack
+    case _ => throw new StackUnderflowException()
+  }
+  val add: (Environment, List[Value]) => List[Value] = {
+    case (_, Num(x) :: Num(y) :: stack) => Num(x + y) :: stack
+    case (_, _ :: _ :: _) => throw new IllegalStateException("[Num, Num] required")
+    case _ => throw new StackUnderflowException()
+  }
+  val subtract: (Environment, List[Value]) => List[Value] = {
+    case (_, Num(x) :: Num(y) :: stack) => Num(x - y) :: stack
+    case (_, _ :: _ :: _) => throw new IllegalStateException("[Num, Num] required")
+    case _ => throw new StackUnderflowException()
+  }
+  val multiply: (Environment, List[Value]) => List[Value] = {
+    case (_, Num(x) :: Num(y) :: stack) => Num(x * y) :: stack
+    case (_, _ :: _ :: _) => throw new IllegalStateException("[Num, Num] required")
+    case _ => throw new StackUnderflowException()
+  }
+  val divide: (Environment, List[Value]) => List[Value] = {
+    case (_, Num(x) :: Num(y) :: stack) => Num(x / y) :: stack
+    case (_, _ :: _ :: _) => throw new IllegalStateException("[Num, Num] required")
+    case _ => throw new StackUnderflowException()
+  }
+  val power: (Environment, List[Value]) => List[Value] = {
+    case (_, Num(x) :: Num(y) :: stack) => Num(Math.pow(x, y)) :: stack
+    case _ => throw new StackUnderflowException()
+  }
+  val equal: (Environment, List[Value]) => List[Value] = {
+    case (_, x :: y :: stack) => Bool(x == y) :: stack
+    case _ => throw new StackUnderflowException()
+  }
+  val greater: (Environment, List[Value]) => List[Value] = {
+    case (_, Num(x) :: Num(y) :: stack) => Bool(x > y) :: stack
+    case _ => throw new StackUnderflowException()
+  }
   val call: (Environment, List[Value]) => List[Value] = {
     case (env, Quote(quote) :: stack) => Evaluator.eval(env, quote, stack)
+    case (_, _ :: _) => throw new IllegalStateException("[Quote] required")
+    case _ => throw new StackUnderflowException()
+  }
+  val `if`: (Environment, List[Value]) => List[Value] = {
+    case (_, alternative :: consequent :: Bool(b) :: stack) => (if (b) { consequent } else { alternative }) :: stack
+    case (_, _ :: _ :: _ :: _) => throw new IllegalStateException("[_, _, Bool] required")
     case _ => throw new StackUnderflowException()
   }
 }
