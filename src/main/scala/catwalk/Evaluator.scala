f56a060a0d5c72f4b3d87c15b9b128b5930e50d4
Robert Koeninger <rkoeninger@att.net>
2017-04-16 00:47:46 -0400

    Bit of cleanup

--- src/main/scala/catwalk/Evaluator.scala
+++ src/main/scala/catwalk/Evaluator.scala
@@ -1,9 +1,12 @@
 package catwalk
 
+import scala.annotation.tailrec
+
 object Evaluator {
+  @tailrec
   def eval(env: Environment, input: List[Value], stack: List[Value]): List[Value] = input match {
     case Nil => stack
-    case Sym(symbol) :: rest => eval(env, rest, app(env, env.verbs(symbol), stack))
+    case Sym(name) :: rest => eval(env, rest, app(env, env verbs name, stack))
     case literal :: rest => eval(env, rest, literal :: stack)
   }
   def app(env: Environment, verb: Verb, stack: List[Value]): List[Value] = verb match {
--- src/main/scala/catwalk/Primitives.scala
+++ src/main/scala/catwalk/Primitives.scala
@@ -2,15 +2,15 @@ package catwalk
 
 object Primitives {
   val swap: (Environment, List[Value]) => List[Value] = {
-    case (_, x :: y :: rest) => y :: x :: rest
+    case (_, x :: y :: stack) => y :: x :: stack
     case _ => throw new StackUnderflowException()
   }
   val drop: (Environment, List[Value]) => List[Value] = {
-    case (_, _ :: rest) => rest
+    case (_, _ :: stack) => stack
     case _ => throw new StackUnderflowException()
   }
   val call: (Environment, List[Value]) => List[Value] = {
-    case (env, Quote(quote) :: rest) => Evaluator.app(env, Definition("anon", quote), rest)
+    case (env, Quote(quote) :: stack) => Evaluator.eval(env, quote, stack)
     case _ => throw new StackUnderflowException()
   }
 }
