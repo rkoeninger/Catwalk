9e99442f883e8195dfefa46413257da0993ba992
Robert Koeninger <rkoeninger@att.net>
2017-04-25 10:56:56 -0400

    Improved exceptions, minor cleanup

--- src/main/scala/catwalk/Primitives.scala
+++ src/main/scala/catwalk/Primitives.scala
@@ -4,172 +4,172 @@ import java.lang.Math.pow
 import Evaluator.eval
 
 object Primitives {
-  val swap: State => State = Pure({
+  val swap: Verb = Pure({
     case x :: y :: stack => y :: x :: stack
     case _ => throw new StackUnderflowException()
   })
-  val swapThird: State => State = Pure({
+  val swapThird: Verb = Pure({
     case x :: y :: z :: stack => z :: y :: x :: stack
     case _ => throw new StackUnderflowException()
   })
-  val swapFourth: State => State = Pure({
+  val swapFourth: Verb = Pure({
     case x :: y :: z :: w :: stack => w :: y :: z :: x :: stack
     case _ => throw new StackUnderflowException()
   })
-  val pull: State => State = Pure({
+  val pull: Verb = Pure({
     case x :: y :: stack => y :: x :: stack
     case _ => throw new StackUnderflowException()
   })
-  val pullThird: State => State = Pure({
+  val pullThird: Verb = Pure({
     case x :: y :: z :: stack => z :: x :: y :: stack
     case _ => throw new StackUnderflowException()
   })
-  val pullFourth: State => State = Pure({
+  val pullFourth: Verb = Pure({
     case x :: y :: z :: w :: stack => w :: x :: y :: z :: stack
     case _ => throw new StackUnderflowException()
   })
-  val drop: State => State = Pure({
+  val drop: Verb = Pure({
     case _ :: stack => stack
     case _ => throw new StackUnderflowException()
   })
-  val duplicate: State => State = Pure({
+  val duplicate: Verb = Pure({
     case x :: stack => x :: x :: stack
     case _ => throw new StackUnderflowException()
   })
-  val add: State => State = Pure({
+  val add: Verb = Pure({
     case Num(x) :: Num(y) :: stack => Num(x + y) :: stack
-    case _ :: _ :: _ => throw new IllegalStateException("[Number, Number, ...] required")
+    case _ :: _ :: _ => throw new UnexpectedStackException(List("Number", "Number"))
     case _ => throw new StackUnderflowException()
   })
-  val subtract: State => State = Pure({
+  val subtract: Verb = Pure({
     case Num(x) :: Num(y) :: stack => Num(x - y) :: stack
-    case _ :: _ :: _ => throw new IllegalStateException("[Number, Number, ...] required")
+    case _ :: _ :: _ => throw new UnexpectedStackException(List("Number", "Number"))
     case _ => throw new StackUnderflowException()
   })
-  val multiply: State => State = Pure({
+  val multiply: Verb = Pure({
     case Num(x) :: Num(y) :: stack => Num(x * y) :: stack
-    case _ :: _ :: _ => throw new IllegalStateException("[Number, Number, ...] required")
+    case _ :: _ :: _ => throw new UnexpectedStackException(List("Number", "Number"))
     case _ => throw new StackUnderflowException()
   })
-  val divide: State => State = Pure({
+  val divide: Verb = Pure({
     case Num(x) :: Num(y) :: stack => Num(x / y) :: stack
-    case _ :: _ :: _ => throw new IllegalStateException("[Number, Number, ...] required")
+    case _ :: _ :: _ => throw new UnexpectedStackException(List("Number", "Number"))
     case _ => throw new StackUnderflowException()
   })
-  val power: State => State = Pure({
+  val power: Verb = Pure({
     case Num(x) :: Num(y) :: stack => Num(pow(x, y)) :: stack
-    case _ :: _ :: _ => throw new IllegalStateException("[Number, Number, ...] required")
+    case _ :: _ :: _ => throw new UnexpectedStackException(List("Number", "Number"))
     case _ => throw new StackUnderflowException()
   })
-  val equal: State => State = Pure({
+  val greater: Verb = Pure({
+    case Num(x) :: Num(y) :: stack => Bool(x > y) :: stack
+    case _ :: _ :: _ => throw new UnexpectedStackException(List("Number", "Number"))
+    case _ => throw new StackUnderflowException()
+  })
+  val equal: Verb = Pure({
     case x :: y :: stack => Bool(x == y) :: stack
     case _ => throw new StackUnderflowException()
   })
-  val greater: State => State = Pure({
-    case Num(x) :: Num(y) :: stack => Bool(x > y) :: stack
-    case _ :: _ :: _ => throw new IllegalStateException("[Number, Number, ...] required")
-    case _ => throw new StackUnderflowException()
-  })
-  val pair: State => State = Pure({
+  val pair: Verb = Pure({
     case x :: y :: stack => Pair(x, y) :: stack
     case _ => throw new StackUnderflowException()
   })
-  val split: State => State = Pure({
+  val split: Verb = Pure({
     case Pair(x, y) :: stack => x :: y :: stack
-    case _ :: _ => throw new IllegalStateException("[Pair, ...] required")
+    case _ :: _ => throw new UnexpectedStackException(List("Pair"))
     case _ => throw new StackUnderflowException()
   })
-  val isBoolean: State => State = Pure({
+  val isBoolean: Verb = Pure({
     case Bool(_) :: stack => Bool(true) :: stack
     case _ :: stack => Bool(false) :: stack
     case _ => throw new StackUnderflowException()
   })
-  val isNumber: State => State = Pure({
+  val isNumber: Verb = Pure({
     case Num(_) :: stack => Bool(true) :: stack
     case _ :: stack => Bool(false) :: stack
     case _ => throw new StackUnderflowException()
   })
-  val isString: State => State = Pure({
+  val isString: Verb = Pure({
     case Str(_) :: stack => Bool(true) :: stack
     case _ :: stack => Bool(false) :: stack
     case _ => throw new StackUnderflowException()
   })
-  val isWord: State => State = Pure({
+  val isWord: Verb = Pure({
     case Word(_) :: stack => Bool(true) :: stack
     case _ :: stack => Bool(false) :: stack
     case _ => throw new StackUnderflowException()
   })
-  val isPair: State => State = Pure({
+  val isPair: Verb = Pure({
     case Pair(_, _) :: stack => Bool(true) :: stack
     case _ :: stack => Bool(false) :: stack
     case _ => throw new StackUnderflowException()
   })
-  val isQuote: State => State = Pure({
+  val isQuote: Verb = Pure({
     case Quote(_) :: stack => Bool(true) :: stack
     case _ :: stack => Bool(false) :: stack
     case _ => throw new StackUnderflowException()
   })
-  val curry: State => State = Pure({
+  val curry: Verb = Pure({
     case x :: Quote(q) :: stack => Quote(x :: q) :: stack
-    case _ :: _ :: _ => throw new IllegalStateException("[Value, Quote, ...] required")
+    case _ :: _ :: _ => throw new UnexpectedStackException(List("Value", "Quote"))
     case _ => throw new StackUnderflowException()
   })
-  val quote: State => State = Pure(stack => Quote(List()) :: stack)
-  val call: State => State = {
-    case State(verbs, Quote(q) :: stack) => eval(q, State(verbs, stack))
-    case State(_, _ :: _) => throw new IllegalStateException("[Quote, ...] required")
-    case _ => throw new StackUnderflowException()
-  }
-  val `if`: State => State = Pure({
+  val quote: Verb = Pure(stack => Quote(List()) :: stack)
+  val `if`: Verb = Pure({
     case y :: x :: Bool(b) :: stack => (if (b) x else y) :: stack
-    case _ :: _ :: _ :: _ => throw new IllegalStateException("[Value, Value, Boolean, ...] required")
+    case _ :: _ :: _ => throw new UnexpectedStackException(List("Value", "Value", "Boolean"))
     case _ => throw new StackUnderflowException()
   })
-  val `try`: State => State = {
+  val call: Verb = Native({
+    case State(verbs, Quote(q) :: stack) => eval(q, State(verbs, stack))
+    case State(_, _ :: _) => throw new UnexpectedStackException(List("Quote"))
+    case _ => throw new StackUnderflowException()
+  })
+  val `try`: Verb = Native({
     case State(verbs, Quote(handler) :: Quote(body) :: stack) =>
       try
         eval(body, State(verbs, stack))
       catch {
+        // TODO: handle EvaluationException specifically?
         case e: Exception => eval(handler, State(verbs, Str(e.getMessage) :: stack))
       }
-    case State(_, _ :: _ :: _) => throw new IllegalStateException("[Quote, Quote, ...] required")
+    case State(_, _ :: _ :: _) => throw new UnexpectedStackException(List("Quote", "Quote"))
     case _ => throw new StackUnderflowException()
-  }
-  // TODO: include new env in exception?
-  val `throw`: State => State = Pure({
-    case Str(message) :: _ => throw new Exception(message)
-    case _ :: _ => throw new IllegalStateException("[String, ...] required")
+  })
+  val `throw`: Verb = Native({
+    case State(verbs, Str(message) :: stack) => throw new EvaluationException(message, State(verbs, stack))
+    case State(_, _:: _) => throw new UnexpectedStackException(List("String"))
     case _ => throw new StackUnderflowException()
   })
   def apply(): Map[String, Verb] = Map[String, Verb](
-    ("swap",        Native(swap)),
-    ("swap third",  Native(swapThird)),
-    ("swap fourth", Native(swapFourth)),
-    ("pull",        Native(pull)),
-    ("pull third",  Native(pullThird)),
-    ("pull fourth", Native(pullFourth)),
-    ("drop",        Native(drop)),
-    ("duplicate",   Native(duplicate)),
-    ("add",         Native(add)),
-    ("subtract",    Native(subtract)),
-    ("multiply",    Native(multiply)),
-    ("divide",      Native(divide)),
-    ("power",       Native(power)),
-    ("equal",       Native(equal)),
-    ("greater",     Native(greater)),
-    ("pair",        Native(pair)),
-    ("split",       Native(split)),
-    ("is boolean",  Native(isBoolean)),
-    ("is number",   Native(isNumber)),
-    ("is string",   Native(isString)),
-    ("is word",     Native(isWord)),
-    ("is pair",     Native(isPair)),
-    ("is quote",    Native(isQuote)),
-    ("curry",       Native(curry)),
-    ("quote",       Native(quote)),
-    ("call",        Native(call)),
-    ("if",          Native(`if`)),
-    ("try",         Native(`try`)),
-    ("throw",       Native(`throw`))
+    ("swap",        swap),
+    ("swap third",  swapThird),
+    ("swap fourth", swapFourth),
+    ("pull",        pull),
+    ("pull third",  pullThird),
+    ("pull fourth", pullFourth),
+    ("drop",        drop),
+    ("duplicate",   duplicate),
+    ("add",         add),
+    ("subtract",    subtract),
+    ("multiply",    multiply),
+    ("divide",      divide),
+    ("power",       power),
+    ("greater",     greater),
+    ("equal",       equal),
+    ("pair",        pair),
+    ("split",       split),
+    ("is boolean",  isBoolean),
+    ("is number",   isNumber),
+    ("is string",   isString),
+    ("is word",     isWord),
+    ("is pair",     isPair),
+    ("is quote",    isQuote),
+    ("curry",       curry),
+    ("quote",       quote),
+    ("if",          `if`),
+    ("call",        call),
+    ("try",         `try`),
+    ("throw",       `throw`)
   )
 }
--- src/main/scala/catwalk/Types.scala
+++ src/main/scala/catwalk/Types.scala
@@ -21,14 +21,19 @@ final case class Native(body: State => State) extends Verb
 final case class Definition(body: List[Value]) extends Verb
 
 object Pure {
-  def apply(f: List[Value] => List[Value]): State => State = {
+  def apply(f: List[Value] => List[Value]): Verb = Native({
     case State(verbs, stack) => State(verbs, f(stack))
-  }
+  })
 }
 
 final case class State(verbs: Map[String, Verb], stack: List[Value]) {
   def define(name: String, verb: Verb) = State(verbs + (name -> verb), stack)
   def push(value: Value) = State(verbs, value :: stack)
 }
+
+class EvaluationException(message: String, val state: State) extends Exception(message)
+
+// TODO: make this an EvaluationException?
+class UnexpectedStackException(top: List[String]) extends Exception(s"[${top.mkString(", ")}] expected")
 
 class StackUnderflowException extends Exception
